a7679228873cd77f6e56cca785a00af95a8ea5bd
Sheetal Temara <stemara@my.okcu.edu>
2016-03-24 19:24:21 -0500

    move modified hello.c to ConsignLite

--- hello.c
+++ hello.c
@@ -3,4 +3,5 @@ main()
 {printf(" This is my first use of git \n");
  printf(" This is my second use of git \n");
  printf(" This is my third use of git \n");
+ printf(" This is the fourth use of git on Lab 9");
 }
